44c1fa776d9962b1acbb3aa940f5403b58529929
Seonho Kim <okeydokekim@gmail.com>
2021-04-12 14:43:17 +0900

    이평선 구하는 기능 추가

--- main.py
+++ main.py
@@ -8,27 +8,45 @@ import pymysql
 from sqlalchemy import create_engine
 
 # DB에 연결
-db_connection = create_engine(pw.conn)
-conn = db_connection.connect()
+engine = create_engine(pw.conn)
+conn = engine.connect()
 
 # 업비트에 연결
 upbit = pyupbit.Upbit(token.access, token.secret)
 
-print(upbit.get_balance("KRW-DOGE"))     # KRW-DOGE 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
-print(pyupbit.get_current_price("KRW-DOGE"))        # 현재 가격
-print(pyupbit.get_ohlcv("KRW-DOGE", interval="minute1", count=1))        # 1분 단위로 시, 고, 저, 종, 거래량 데이터 count만큼 가져오기
+# # KRW-DOGE 조회
+# print(upbit.get_balance("KRW-DOGE")
+# # 보유 현금 조회
+# print(upbit.get_balance("KRW"))
+# # 현재 가격
+# print(pyupbit.get_current_price("KRW-DOGE"))
+# # 1분 단위로 시, 고, 저, 종, 거래량 데이터 count만큼 가져오기
+# print(pyupbit.get_ohlcv("KRW-DOGE", interval="minute1", count=1))
 
-# DB에 1분마다 거래소 데이터 입력
-def dataInsert():
-    df = pyupbit.get_ohlcv("KRW-DOGE", interval="minute1", count=1)
-    df.to_sql(name='1_minute_data', con=db_connection, if_exists='append', index=False)
+# 이평선
+def ma():
+    sql = 'SELECT * FROM 1_minute_data ORDER BY date DESC LIMIT 20;'
+    df = pd.read_sql(sql, con=engine)   
     print(df)
+    ma_5 = 0
+    ma_20 = 0
+
+    if len(df) == 20:
+        sum = 0
+        for i in range(0, 5):
+            sum += df.open[i]
+        ma_5 = sum/5
+        for i in range(5, 20):
+            sum += df.open[i]
+        ma_20 = sum/20
+        return {5: ma_5, 20: ma_20}
+
+    return None
 
 
-schedule.every(60).second.do(dataInsert) # 60초마다 실행
-schedule.every(1).second.do(dataInsert) # 1초마다 실행
+# # 1초마다 실행
+# schedule.every(1).second.do()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
