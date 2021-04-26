e91fa4f2c20c11c97943c73e38d63b288da919d1
Seonho Kim <okeydokekim@gmail.com>
2021-04-26 12:15:01 +0900

    상승장 투자 전략 구현

--- back_test.py
+++ back_test.py
@@ -9,19 +9,20 @@ import math
 upbit = pyupbit.Upbit(token.access, token.secret)
 
 # 테스트 설정 
-test_length = 5
-test_data_interval = "week" # day/minute1/minute3/minute5/minute10/minute15/minute30/minute60/minute240/week/month
-test_end_date = None # None으로 하면 현재까지
+test_length = 21
+test_data_interval = "day" # day/minute1/minute3/minute5/minute10/minute15/minute30/minute60/minute240/week/month
+test_end_date = None # "20210201"/None (None으로 하면 현재까지)
 start_money = 100000.0
 test_money = start_money
 bougth_price = 0;
 test_coin = 0.0
 fee = 0.0005
-slippage = 0.01
+slippage = 0.001
 coin = "KRW-XRP"
 
 print("------------------------------- Test Start -------------------------------")
 
+# 데이터 분석을 위한 테이터 가져오기
 def get_data():
     date = test_end_date
     dfs = [ ]
@@ -44,13 +45,17 @@ def get_data():
     df = pd.concat(dfs).sort_index()
     return df
 
+# 레리 윌리엄스의 변동성 돌파 전략, K값에 따른 수익률
 def larry_ror(df_, k):
     
     df = df_
+
+    df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
+    df['bull'] = df['open'] > df['ma5']
     df['range'] = (df['high'] - df['low']) * k
     df['range_shift1'] = df['range'].shift(1)
     df['target'] = df['open'] + df['range'].shift(1)
-    df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'] - (fee + fee + slippage), 1)
+    df['ror'] = np.where((df['high'] > df['target']) & df['bull'], df['close'] / df['target'] - (fee + fee + slippage), 1)
     df['hpr'] = df['ror'].cumprod()
     ror = df['hpr'][-2]
 
@@ -61,21 +66,24 @@ def larry_ror(df_, k):
 
 data = get_data()
 
+# K값에 따른 수익률이 높은 순서대로 리스트에 정렬하여 출력
 ror_list = []
-for k in np.arange(0.001, 1.000, 0.001):
+for k in np.arange(0.000, 1.000, 0.001):
     ror = larry_ror(data, k)
     ror_list.append([round(k, 5), round((ror - 1) * 100, 2)])
 
 ror_list.sort(key = lambda x:x[1])
 
 for i in ror_list:
-    print("- k: ", i[0], " / 수익률: ", i[1], " %")
+    print("- K: ", i[0], " / 수익률: ", i[1], "%")
 
+# 테스트 결과 출력
 print("\n")
 print("Interval: ", test_data_interval)
 print("테스트시작: ", data.index[0])
 print("테스트종료: ", data.index[len(data)-1])
 print("시작가: ", data.close[0])
 print("종료가: ", data.close[len(data)-1])
-print("존버 시 수익률: ", round(((data.close[len(data)-1] - data.close[0]) / data.close[0])*100, 3), " %")
+print("존버 시 수익률: ", round(((data.close[len(data)-1] - data.close[0]) / data.close[0])*100, 3), "%")
+print("프로그램 최고 수익률: ", ror_list[len(ror_list) - 1][1], "%", "/ K =", ror_list[len(ror_list) - 1][0])
 print("-------------------------------  Test End  -------------------------------")
