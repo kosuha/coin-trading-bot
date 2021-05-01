fa2639d798ba177ada4d56d9f34f40a9aefe31f8
Seonho Kim <okeydokekim@gmail.com>
2021-05-01 16:41:20 +0900

    이평선 테스트

--- back_test.py
+++ back_test.py
@@ -9,15 +9,15 @@ import math
 upbit = pyupbit.Upbit(token.access, token.secret)
 
 # 테스트 설정 
-test_length = 300 * 12 * 24
-test_data_interval = "minute5" # day/minute1/minute3/minute5/minute10/minute15/minute30/minute60/minute240/week/month
+test_length = 365
+test_data_interval = "day" # day/minute1/minute3/minute5/minute10/minute15/minute30/minute60/minute240/week/month
 test_end_date = None # "20210201"/None (None으로 하면 현재까지)
-start_money = 1000000
+start_money = 100000
 test_money = start_money - 5000
 bougth_price = 0;
 test_coin = 0.0
 fee = 0.0005
-slippage = 0.0015
+slippage = 0.001
 coin = "KRW-XRP"
 currency = "KRW"
 K = 0.0
@@ -28,7 +28,7 @@ print("------------------------------- Test Start ------------------------------
 def get_data():
     date = test_end_date
     dfs = [ ]
-    length = test_length + 5
+    length = test_length + 20
 
     if length > 200:
         loop_num = length // 200
@@ -54,15 +54,31 @@ def larry_ror(df_, k):
     df = df_
 
     df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
+    df['ma10'] = df['close'].rolling(window=10).mean().shift(1)
     df['bull'] = df['open'] > df['ma5']
     df['range'] = (df['high'] - df['low']) * k
     df['range_shift1'] = df['range'].shift(1)
+    # df['is_minus'] = np.where(df['open'] > df['close'], True, False)
+    # df['is_minus_shift1'] = df['is_minus'].shift(1)
     df['target'] = df['open'] + df['range'].shift(1)
+    df['sell_condition'] = np.where(df['bull'] == False, True, False)
+    # df['buy_condition'] = np.where((df['ma5'] > df['ma10']) & (df['sell_condition'] == False), True, False)
     df['buy_condition'] = np.where((df['high'] > df['target']) & df['bull'], True, False)
-    df['buy_condition'] = np.where((df['high'] > df['target']) & df['bull'], True, False)
-    df['buy_condition_shift1'] = df['buy_condition'].shift(1)
-    df['ror_trading'] = np.where(df['buy_condition'] & (df['buy_condition_shift1'] == False), df['close'] / df['target'] - (fee + fee + slippage), df['close'] / df['target'])
-    df['ror'] = np.where(df['buy_condition'] & df['bull'], df['ror_trading'], 1)
+    
+    bought_status = False
+    for i in df.index:
+        if df.at[i, 'buy_condition']:
+            bought_status = True
+        if df.at[i, 'sell_condition']:
+            bought_status = False
+        df.loc[i, 'bought'] = bought_status
+
+    df['bought_shift1'] = df['bought'].shift(1)
+    df['bought_shift1'].fillna(False, inplace = True)
+    df['ror'] = np.where(df['bought'] == True, df['close'] / df['target'], 1)
+    df['ror'] = np.where((df['bought'] == True) & (df['bought_shift1'] == False), df['close'] / df['target'] - (fee + slippage), df['ror'])
+    df['ror'] = np.where((df['bought'] == False) & (df['bought_shift1'] == True), 1 - (fee + slippage), df['ror'])
+
     df['hpr'] = df['ror'].cumprod()
     ror = df['hpr'][-1]
 
@@ -94,11 +110,11 @@ print("\n")
 print("Start Money: ", format(round(start_money), ","), currency)
 print("Interval: ", test_data_interval)
 print("테스트 기간: ", test_length)
-print("테스트 시작: ", data.index[5])
+print("테스트 시작: ", data.index[20])
 print("테스트 종료: ", data.index[len(data)-1])
-print("시작가: ", data.close[5])
+print("시작가: ", data.open[20])
 print("종료가: ", data.close[len(data)-1])
-print("존버 시 수익률: ", round(((data.close[len(data)-1] - data.close[5]) / data.close[5])*100, 3), "%")
+print("존버 시 수익률: ", round(((data.close[len(data)-1] - data.open[20]) / data.open[20])*100, 3), "%")
 print("프로그램 최고 수익률: ", ror_list[len(ror_list) - 1][1], "%", "/ K =", ror_list[len(ror_list) - 1][0])
 print("K 값 수익률: ", check_K[0][1], "%", "/ K =", check_K[0][0])
 print("End Money: ", format(round(start_money + ((start_money - 5000) * check_K[0][1] * 0.01)), ","), currency)
