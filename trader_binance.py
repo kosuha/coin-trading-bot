38385bf0fd8a66f4bda3bff749ffc41e4045d172
kosuha <okeydokekim@gmail.com>
2022-02-19 12:19:58 +0900

    bug fix

--- trader_binance.py
+++ trader_binance.py
@@ -121,7 +121,7 @@ def main():
     total = total_balance()
     rsi_up = 76
     rsi_down = 24
-    entry_count = 0
+    entry_count = 2
     entry_max = 10
 
     slack_bot.post_message(f"#\nStart Binance Futures Trading.\nStart balance: {round(total, 2)} USDT\nTicker: {ticker}")
