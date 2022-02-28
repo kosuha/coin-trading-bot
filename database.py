4a45c11f3c7b3a629da6d8fe5ed117bc907f741d
kosuha <okeydokekim@gmail.com>
2022-02-28 23:53:45 +0900

    fix bug

--- database.py
+++ database.py
@@ -2,19 +2,21 @@ import pymysql
 import config.conn as conn
 import datetime
 
-db = pymysql.Connect(
-	host=conn.host,
-	user=conn.user,
-	password=conn.password,
-	database=conn.database
-)
+
 
 def insert_data(balance, price):
 	now = datetime.datetime.now()
 	now_datetime = f"{now.year}-{format(now.month, '02')}-{format(now.day, '02')} {format(now.hour, '02')}:{format(now.minute, '02')}"
 
+	db = pymysql.Connect(
+		host=conn.host,
+		user=conn.user,
+		password=conn.password,
+		database=conn.database
+	)
 	cursor = db.cursor()
 	query = "INSERT INTO history (date, balance, price) VALUES(%s, %s, %s)"
 	data = (now_datetime, balance, price)
 	cursor.execute(query, data)
 	db.commit()
+	db.close()
